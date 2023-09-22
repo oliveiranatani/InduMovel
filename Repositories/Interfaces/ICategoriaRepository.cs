3a2c194f77a2af5df5934dde7bb24dc3795e9cc9
Aluno Dev <natani.neves@portalsesisp.org.br>
2023-09-22 17:09:16 -0300

    Repositories

---
+++ Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+using InduMovel.Models;
+namespace InduMovel.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        public IEnumerable<Categoria> Categorias { get; }
+    }
+}
+
